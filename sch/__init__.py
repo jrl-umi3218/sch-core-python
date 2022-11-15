fb5bc5d789986df53844d8f0620db95f349e4096
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-11-15 21:25:27 +0900

    Add the Boost DLL directory to the search path as well

--- sch/__init__.py
+++ sch/__init__.py
@@ -1,5 +1,6 @@
 import os
 if hasattr(os, 'add_dll_directory'):
     os.add_dll_directory("$<TARGET_FILE_DIR:sch-core::sch-core>")
+    os.add_dll_directory("$<TARGET_FILE_DIR:Boost::serialization>")
 
 from . sch import *
